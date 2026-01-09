6d2f89340affb99a5a7ad01dedef61b81e4ba923
ShixinPeng <58160712+BCeZn@users.noreply.github.com>
2026-01-09 16:39:22 +0800

    Update CI with self-hosted (#219)
    * ci: change CI runner from ubuntu-latest to self-hosted
    
    * ci: comment out Python setup and installation steps
    
    * ci: enable Docker checks in CI workflow
    
    * refactor: update remote user test marks and conditions
    
    * ci: comment out Docker check steps in CI yaml
    
    * ci: update Python version for CI setu
    
    * ci: simplify Python CI workflow matrix and steps
    
    * ci: update python ci workflow for ray tests

--- tests/integration/sdk/sandbox/test_remote_user.py
+++ tests/integration/sdk/sandbox/test_remote_user.py
@@ -5,16 +5,21 @@ import pytest
 from rock.actions.sandbox.request import CreateBashSessionRequest
 from rock.actions.sandbox.response import Observation
 from rock.sdk.sandbox.client import Sandbox
+from tests.integration.sdk.sandbox.test_basic import SKIP_IF_NO_DOCKER
 
 logger = logging.getLogger(__name__)
 
 
+@pytest.mark.need_admin
+@SKIP_IF_NO_DOCKER
 @pytest.mark.asyncio
 async def test_remote_user_create(sandbox_instance: Sandbox):
     assert await sandbox_instance.remote_user.create_remote_user("rock")
     assert await sandbox_instance.remote_user.is_user_exist("rock")
 
 
+@pytest.mark.need_admin
+@SKIP_IF_NO_DOCKER
 @pytest.mark.asyncio
 async def test_create_session_with_remote_user(sandbox_instance: Sandbox):
     assert await sandbox_instance.remote_user.create_remote_user("rock")
